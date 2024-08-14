a178e5f317bc885c33d8efef22502260a9cc10e5
Philip Paetz <philip.paetz@me.com>
2024-08-14 18:46:20 +0200

    chore: merge set-project-id.sh and set-project-vars.sh

--- set-project-vars.sh
+++ set-project-vars.sh
@@ -3,6 +3,57 @@ set -e          # Fail on any error
 set -o pipefail # Ensure piped commands propagate exit codes properly
 set -u          # Treat unset variables as an error when substituting
 
+source ./check-gcloud-login.sh
+
+set_project_id() {
+	printf "Looking up terraform workspace..."
+	workspace=$(terraform -chdir=infra workspace show)
+	printf ' \033[1m%s\033[0m\n' "${workspace}"
+
+	printf "Looking up project name in variables.tf..."
+	project_name=$(awk '/variable "project_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')-${workspace}
+	printf ' \033[1m%s\033[0m\n' "${project_name}"
+
+	printf "Fetching the project ID..."
+	project_id=$(gcloud projects list --filter="name:${project_name}" --format="value(projectId)")
+	printf ' \033[1m%s\033[0m\n' "${project_id}"
+
+	# Set your local default project
+	printf "Setting your default project to \033[1m%s\033[0m...\n" "${project_id}"
+	{
+		output=$(gcloud config set project "${project_id}" 2>&1 >/dev/null)
+		status=$?
+	}
+	if [[ ${status} -ne 0 ]]; then
+		echo "Error: ${output}"
+		return "${status}"
+	fi
+
+	# Set the quota project to the governance-watchdog project, some gcloud commands require this to be set
+	printf "Setting the quota project to \033[1m%s\033[0m...\n" "${project_id}"
+	{
+		output=$(gcloud auth application-default set-quota-project "${project_id}" 2>&1 >/dev/null)
+		status=$?
+	}
+	if [[ ${status} -ne 0 ]]; then
+		echo "Error: ${output}"
+		return "${status}"
+	fi
+
+	# Update the project ID in your .env file so your cloud function points to the correct project when running locally
+	printf "Updating the project ID in your .env file...\n\n"
+	# Check if .env file exists
+	if [[ ! -f .env ]]; then
+		# If .env doesn't exist, create it with the initial value
+		echo "GCP_PROJECT_ID=${project_id}" >.env
+	else
+		# If .env exists, perform the sed replacement
+		sed -i '' "s/^GCP_PROJECT_ID=.*/GCP_PROJECT_ID=${project_id}/" .env
+	fi
+
+	echo "✅ All Done!"
+}
+
 cache_file=".project_vars_cache"
 
 # Function to load values from cache
@@ -34,12 +85,12 @@ write_cache() {
 fetch_values() {
 	printf "Loading and caching project values...\n\n"
 
-	printf " - Project ID:"
-	project_id=${current_tf_state_project_id}
-	printf ' \033[1m%s\033[0m\n' "${project_id}"
+	printf " - Terraform Workspace"
+	workspace=$(terraform -chdir=infra workspace show)
+	printf ' \033[1m%s\033[0m\n' "${workspace}"
 
 	printf " - Project Name:"
-	project_name=$(awk '/variable "project_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')
+	project_name=$(awk '/variable "project_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')-${workspace}
 	printf ' \033[1m%s\033[0m\n' "${project_name}"
 
 	printf " - Region:"
@@ -47,11 +98,11 @@ fetch_values() {
 	printf ' \033[1m%s\033[0m\n' "${region}"
 
 	printf " - Service Account:"
-	service_account_email=$(terraform -chdir=infra state show "module.project-factory.module.project-factory.google_service_account.default_service_account[0]" | grep email | awk '{print $3}' | tr -d '"')
+	service_account_email=$(terraform -chdir=infra state show "module.oracle_relayer.module.project-factory.google_service_account.default_service_account[0]" | grep email | awk '{print $3}' | tr -d '"')
 	printf ' \033[1m%s\033[0m\n' "${service_account_email}"
 
 	printf " - Function Name:"
-	function_name=$(awk '/variable "function_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')
+	function_name=$(awk '/variable "function_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')-${workspace}
 	printf ' \033[1m%s\033[0m\n' "${function_name}"
 
 	printf " - Function Entry Point:"
@@ -59,11 +110,11 @@ fetch_values() {
 	printf ' \033[1m%s\033[0m\n' "${function_entry_point}"
 
 	printf " - Pubsub Topic:"
-	topic_name=$(awk '/variable "pubsub_topic"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')
+	topic_name=$(awk '/variable "pubsub_topic"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')-${workspace}
 	printf ' \033[1m%s\033[0m\n' "${topic_name}"
 
 	printf " - Scheduler Job:"
-	scheduler_job_name=$(awk '/variable "scheduler_job_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')
+	scheduler_job_name=$(awk '/variable "scheduler_job_name"/{f=1} f==1&&/default/{print $3; exit}' ./infra/variables.tf | tr -d '",')-${workspace}
 	printf ' \033[1m%s\033[0m\n' "${scheduler_job_name}"
 
 	printf "\nCaching values in"
@@ -80,27 +131,38 @@ invalidate_cache() {
 }
 
 # Main script logic
-current_local_project_id=$(gcloud config get project)
-current_tf_state_project_id=$(terraform -chdir=infra state show module.project-factory.module.project-factory.google_project.main | grep project_id | awk '{print $3}' | tr -d '"')
-
 main() {
+	check_gcloud_login
+
+	printf "Loading current local gcloud project ID: "
+	current_local_project_id=$(gcloud config get project)
+	printf ' \033[1m%s\033[0m\n' "${current_local_project_id}"
+
+	printf "Comparing with project ID from terraform state: "
+	# current_tf_state_project_id=$(terraform -chdir=infra state show module.oracle_relayer.module.project-factory.google_project.main | grep project_id | awk '{print $3}' | tr -d '"')
+	current_tf_state_project_id=$(terraform -chdir=infra state show module.oracle_relayer.module.project-factory.google_project.main 2>/dev/null | grep project_id | awk '{print $3}' | tr -d '"' || echo "Not found")
+	printf ' \033[1m%s\033[0m\n\n' "${current_tf_state_project_id}"
+
 	if [[ ${current_local_project_id} != "${current_tf_state_project_id}" ]]; then
 		printf '️\n🚨 Your local gcloud is set to the wrong project: \033[1m%s\033[0m 🚨\n' "${current_local_project_id}"
-		printf "\nRunning ./set-project-id.sh in an attempt to fix this...\n\n"
-		source ./set-project-id.sh
-		printf "\n\n"
+		printf "\nTrying to set the correct project...\n\n"
+		set_project_id
 		invalidate_cache
 		fetch_values
-		return
+		printf "\n\n"
+		return 0
+	else
+		project_id="${current_local_project_id}"
 	fi
 
 	if [[ ${1-} == "--invalidate-cache" ]]; then
 		invalidate_cache
 	fi
 
-	# Separate the invocation of load_cache from the if condition
+	set +e # Disable exit on error
 	load_cache
 	cache_loaded=$?
+	set +e # Re-enable exit on error
 
 	if [[ ${cache_loaded} -eq 0 ]]; then
 		printf "Using cached values from %s:\n" "${cache_file}"
