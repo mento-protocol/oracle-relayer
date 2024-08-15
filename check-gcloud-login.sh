52e61cc7a2b1b8103b9e539ebc81454450ef49a8
Philip Paetz <philip.paetz@me.com>
2024-08-15 19:55:40 +0200

    chore: added gcloud login checker script

---
+++ check-gcloud-login.sh
@@ -0,0 +1,24 @@
+check_gcloud_login() {
+	echo "🌀 Checking gcloud login..."
+	# Check if there's an active account
+	if ! gcloud auth list --filter=status:ACTIVE --format="value(account)" | grep -q .; then
+		echo "No active Google Cloud account found. Initiating login..."
+		gcloud auth login
+		echo "✅ Successfully logged in to gcloud"
+	else
+		echo "ℹ️  Already logged in to Google Cloud."
+	fi
+	printf "\n"
+
+	echo "🌀 Checking gcloud application-default credentials..."
+	if ! gcloud auth application-default print-access-token &>/dev/null; then
+		echo "No valid application-default credentials found. Initiating login..."
+		gcloud auth application-default login
+		echo "✅ Successfully logged in to gcloud"
+	else
+		echo "ℹ️  Already logged in with valid application-default credentials."
+	fi
+	printf "\n"
+}
+
+check_gcloud_login
