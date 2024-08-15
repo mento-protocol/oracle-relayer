fc5c29428adaf4bbea972b4945891dc076c447cd
Philip Paetz <philip.paetz@me.com>
2024-08-15 20:09:17 +0200

    feat: made scheduler job creation dynamic
    it now reads the relayer config from a
    json file and will iterate over every
    address it finds and create one relayer
    job per address

--- infra/scheduler.tf
+++ infra/scheduler.tf
@@ -1,17 +1,19 @@
-# Create PHP/USD Scheduler job
-resource "google_cloud_scheduler_job" "php_usd" {
-  project     = module.project-factory.project_id
+# Create Scheduler jobs to trigger the Cloud Function for each relayer address
+resource "google_cloud_scheduler_job" "relay_jobs" {
+  # This will iterate over the relayer addresses and create one scheduler job for each
+  for_each = local.relayer_addresses[terraform.workspace]
+
+  project     = module.oracle_relayer.project_id
   region      = var.region
-  name        = var.scheduler_job_name
-  description = "Emits a 'RelayRequested' event to Pub/Sub every minute with a rate feed name and a relayer address as parameters."
+  name        = "${var.scheduler_job_name}-${each.key}-${terraform.workspace}"
+  description = "Emits a 'RelayRequested' event to Pub/Sub every minute for ${each.key} with a relayer address as parameter."
   schedule    = "* * * * *" # Run every minute
 
   pubsub_target {
     topic_name = google_pubsub_topic.relay_requested.id
     data = base64encode(jsonencode({
-      rate_feed_name = "PHP/USD"
-      # TODO: Replace with the actual relayer address for PHP/USD after it's deployed
-      relayer_address = "0x3005a33a9782f4c1ccfa0ffdb87a034b95b7ad90"
+      rate_feed_name  = upper(replace(each.key, "_", "/"))
+      relayer_address = each.value
     }))
   }
 }
