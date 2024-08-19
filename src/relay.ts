4d32c1ff5e3065053bc66324cd81c504f3cd3bed
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2024-08-19 11:50:44 +0200

    refactor: better error handling/logs upon tx failure (#11)
    A little cleanup over the error handling logic to have better separation
    over the custom errors that we know the relayer contract can throw.
    
    ---------
    
    Co-authored-by: chapati <philip.paetz@me.com>

--- src/relay.ts
+++ src/relay.ts
@@ -66,31 +66,58 @@ export default async function relay(
     logger.info(`Relay succeeded: ${hash}`);
     return true;
   } catch (err) {
-    if (err instanceof BaseError) {
-      const revertError = err.walk(
-        (err) => err instanceof ContractFunctionRevertedError,
-      );
-      if (revertError instanceof ContractFunctionRevertedError) {
-        const errName = revertError.data?.errorName ?? "";
-        if (errName !== "Error") {
-          // One of the custom errors we defined, which should include
-          // a more detailed message in the metaMessages field
-          const metaMsg = revertError.metaMessages
-            ?.map((msg: string) => msg.trim())
-            .join("");
-
-          logger.error("Contract reverted with error:", metaMsg);
-        } else {
-          // A generic revert error, which should include the reason
-          logger.error("Contract reverted with reason:", revertError.reason);
-        }
-      } else {
-        // Non-revert error, for example not enough balance, tx broadcast timeout, etc.
-        logger.error(`Error relaying tx: ${err.shortMessage}`);
-      }
-    } else {
-      logger.error(`Unknown error relaying tx:`, err);
+    if (!(err instanceof BaseError)) {
+      // Theoretically should never happen, as all errors in Viem should extend BaseError
+      logger.error("Relay failed due to an unknown non-BaseError:", err);
+      return false;
     }
+
+    const revertError = err.walk(
+      (err) => err instanceof ContractFunctionRevertedError,
+    );
+    if (revertError instanceof ContractFunctionRevertedError) {
+      // If the error was a revert from the contract, it should fall into one of two types:
+      // 1. A custom error defined in the relayer contract, i.e. InvalidPrice, TimestampNotNew, etc.
+      // 2. An error from a require statement, in which case we try to extract the reason
+      const errName = revertError.data?.errorName ?? "";
+      switch (errName) {
+        case "TimestampNotNew": {
+          logger.info(
+            "Relay skipped. Price from previous relay is still fresh in SortedOracles",
+          );
+          break;
+        }
+        case "ExpiredTimestamp": {
+          logger.warn(
+            "Relay not possible. The current price is too old to be relayed",
+          );
+          break;
+        }
+        case "InvalidPrice": {
+          logger.error("Relay failed. Chainlink price is invalid");
+          break;
+        }
+        case "Error": {
+          logger.error(
+            "Relay failed. Contract reverted with:",
+            revertError.reason,
+          );
+          break;
+        }
+        default: {
+          logger.error(
+            `Relay failed. Unknown error type: ${errName} - ${revertError.shortMessage}`,
+          );
+          break;
+        }
+      }
+      return false;
+    }
+
+    // At this point we know that the error is not a revert from the contract, so it could be an error
+    // from the rpc client, i.e. not enough balance, incorrect nonce, tx broadcast timeout, etc,. in
+    // which case the shortMessage should be descriptive enough
+    logger.error(`Relay failed with a non-revert error: ${err.shortMessage}`);
 
     return false;
   }
