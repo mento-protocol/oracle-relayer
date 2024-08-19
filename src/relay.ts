4f605083a0711f790071137c4a1409ecbe4b9b4b
chapati <philip.paetz@me.com>
2024-08-19 11:45:07 +0200

    Update src/relay.ts

--- src/relay.ts
+++ src/relay.ts
@@ -83,7 +83,7 @@ export default async function relay(
       switch (errName) {
         case "TimestampNotNew": {
           logger.info(
-            "Relay not possible. Price from previous relay is still fresh in SortedOracles",
+            "Relay skipped. Price from previous relay is still fresh in SortedOracles",
           );
           break;
         }
