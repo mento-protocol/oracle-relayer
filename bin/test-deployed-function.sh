eaf6299c1ecdff3b70a58391f8205602fbdc6d0b
Philip Paetz <philip.paetz@me.com>
2024-08-20 17:25:18 +0200

    chore: update test-deployed-function script to accept a rate feed name as param

--- bin/test-deployed-function.sh
+++ bin/test-deployed-function.sh
@@ -6,24 +6,47 @@ set -u          # Treat unset variables as an error when substituting
 # Manually triggers a deployed Cloud Function by emitting a Pubsub event.
 # Requires an environment arg (e.g., staging, production).
 test_deployed_function() {
-	# Check if environment parameter is provided
-	if [[ $# -ne 1 ]]; then
-		echo "Usage: $0 <env>"
-		echo "Example: $0 staging"
+	# Check if environment and rate feed ID parameters are provided
+	if [[ $# -ne 2 ]]; then
+		echo "Usage: $0 <env> <rate_feed_id>"
+		echo "Example: $0 staging PHP/USD"
 		exit 1
 	fi
 	env=$1
+	rate_feed_id=$2
+
+	# Validate rate feed ID format
+	if ! [[ ${rate_feed_id} =~ ^[A-Z]{3,4}/[A-Z]{3,4}$ ]]; then
+		echo "❌ Error: Invalid rate feed ID format. It should be in the form of 'PHP/USD' (3-4 digits per currency symbol)."
+		exit 1
+	fi
+
+	json_key=$(echo "${rate_feed_id}" | tr '[:upper:]' '[:lower:]' | tr '/' '_')
 
 	terraform -chdir=infra workspace select "${env}"
 
 	# Load the project variables
 	script_dir=$(dirname "$0")
 	source "${script_dir}/get-project-vars.sh"
+	printf "\n"
+
+	# Read the relayer addresses JSON file and extract the address
+	json_file="infra/relayer_addresses.json"
+	if [[ ! -f ${json_file} ]]; then
+		echo "❌ Error: ${json_file} file not found in the script directory."
+		exit 1
+	fi
+
+	address=$(jq -r ".${env}.\"${json_key}\"" "${json_file}")
+
+	if [[ ${address} == "null" ]]; then
+		echo "❌ Error: Address not found for rate feed ID '${rate_feed_id}' in environment '${env}'."
+		exit 1
+	fi
 
 	echo "🌀 Emitting a Pubsub event to trigger the function..."
 	printf "\n"
-	# TODO: Update the relayer address to a real one once we have one deployed in prod
-	gcloud pubsub topics publish "${topic_name}" --message='{"rate_feed_name": "TEST/USD", "relayer_address": "0x13374935239dacdecf7c5ba76d8de40b077b7420"}'
+	gcloud pubsub topics publish "${topic_name}" --message='{"rate_feed_name": "PHP/USD", "relayer_address": "0xF93c6fe760F09f19880f57D643a17A515c11165c"}'
 	printf "\n"
 
 	echo "✅ Pubsub event emitted!"
