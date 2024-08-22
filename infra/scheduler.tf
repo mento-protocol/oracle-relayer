04760e0d6157a2fc55b130da38c0b2a47b79d437
Philip Paetz <philip.paetz@me.com>
2024-08-22 11:10:58 +0200

    chore: updated scheduler description with relayer address value

--- infra/scheduler.tf
+++ infra/scheduler.tf
@@ -6,7 +6,7 @@ resource "google_cloud_scheduler_job" "relay_jobs" {
   project     = module.oracle_relayer.project_id
   region      = var.region
   name        = "${var.scheduler_job_name}-${each.key}-${terraform.workspace}"
-  description = "Emits a 'RelayRequested' event to Pub/Sub every minute for ${each.key} with a relayer address as parameter."
+  description = "Emits a 'RelayRequested' event for ${each.key} to Pub/Sub every minute with the relayer address ${each.value}"
   schedule    = "* * * * *" # Run every minute
 
   pubsub_target {
