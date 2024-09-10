5d8711acd3276d7543ab45382439ab80a79dc395
Philip Paetz <philip.paetz@me.com>
2024-09-10 12:53:52 +0200

    feat: added aegis export script

---
+++ bin/aegis-export.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e          # Fail on any error
+set -o pipefail # Ensure piped commands propagate exit codes properly
+set -u          # Treat unset variables as an error when substituting
+
+# If .env exists, source it
+if [[ -f .env ]]; then
+	source .env
+	# Check if RELAYER_MNEMONIC_SECRET_ID is set, if not, regenerate .env
+	if [[ -z ${RELAYER_MNEMONIC_SECRET_ID} ]]; then
+		echo 'Env var RELAYER_MNEMONIC_SECRET_ID not found. Regenerating .env...'
+		npm run generate:env
+		source .env
+	fi
+
+# If not, regenerate .env to ensure RELAYER_MNEMONIC_SECRET_ID is set
+else
+	echo 'Env var RELAYER_MNEMONIC_SECRET_ID not found. Regenerating .env...'
+	npm run generate:env
+fi
+
+# Print out all signer wallets for all relayers in an aegis-compatible format (so monitoring additional signer wallets becomes as easy as copy-pasting)
+echo "Generating aegis-compatible signer wallet export..."
+NODE_ENV=development npx ts-node ./src/aegis-export.ts
---
+++ src/aegis-export.ts
@@ -0,0 +1,105 @@
+import type { Address } from "viem";
+import RelayerAddressesJson from "../infra/relayer_addresses.json";
+import { config } from "./config";
+import getSecret from "./get-secret";
+import { deriveRelayerAccount, toRateFeedId } from "./utils";
+
+const RelayerAddresses = RelayerAddressesJson as {
+  [env in "staging" | "prod"]: Record<string, Address>;
+};
+
+type Environment = keyof typeof RelayerAddresses;
+type RateFeed<T extends Environment> = keyof (typeof RelayerAddresses)[T];
+
+interface Relayer {
+  env: Environment;
+  rateFeedId: Address;
+  rateFeed: string;
+  relayerAddress: Address;
+  signerAddress: Address;
+}
+
+async function aegisExport() {
+  try {
+    const mnemonic = await getSecret(config.RELAYER_MNEMONIC_SECRET_ID);
+
+    const allRelayers = (
+      Object.entries(RelayerAddresses) as [
+        Environment,
+        (typeof RelayerAddresses)[Environment],
+      ][]
+    ).flatMap(([env, rateFeeds]) =>
+      Object.keys(rateFeeds).map((rateFeed) =>
+        createRelayer(env, rateFeed, mnemonic),
+      ),
+    );
+
+    const uniqueRelayers = Array.from(
+      new Map(
+        allRelayers.map((relayer) => [relayer.rateFeedId, relayer]),
+      ).values(),
+    );
+
+    const configYaml = generateConfigYaml(uniqueRelayers);
+
+    console.log("\x1b[1m" + configYaml + "\x1b[0m");
+  } catch (error) {
+    console.error("Error occurred during aegis export:", error);
+    process.exit(1);
+  }
+}
+
+const createRelayer = <T extends Environment>(
+  env: T,
+  rateFeed: RateFeed<T>,
+  mnemonic: string,
+): Relayer => {
+  // Type assertion (as string) is safe here because we know that the keys of our RelayerAddresses object are always strings
+  const formattedRateFeed = (rateFeed as string).replace("_", "").toUpperCase();
+  return {
+    env,
+    rateFeed: formattedRateFeed,
+    rateFeedId: toRateFeedId(`relayed:${formattedRateFeed}`),
+    signerAddress: deriveRelayerAccount(mnemonic, rateFeed as string).address,
+    relayerAddress: RelayerAddresses[env][rateFeed] as Address,
+  };
+};
+
+function generateConfigYaml(relayers: Relayer[]): string {
+  return `
+###############################################################
+# Exemplary aegis config.yaml with all relevant values to add #
+###############################################################
+
+global:
+  vars:
+    # Rate Feed IDs
+${relayers.map(({ rateFeed, rateFeedId }) => `    'relayed:${rateFeed}': '${rateFeedId}'`).join("\n")}
+
+    # Relayer Signer Wallets
+${relayers.map(({ rateFeed, signerAddress }) => `    RelayerSigner${rateFeed}: '${signerAddress}'`).join("\n")}
+
+metrics:
+  # Checks for rate feed freshness
+  - source: SortedOracles.isOldestReportExpired(address rateFeed)(bool,address)
+    schedule: 0/10 * * * * *
+    type: gauge
+    chains: all
+    variants:
+${relayers.map(({ rateFeed }) => `      - [relayed:${rateFeed}]`).join("\n")}
+
+  # Checks if the signer wallets have enough CELO to pay for the relay() transactions
+  - source: CELOToken.balanceOf(address owner)(uint256)
+    schedule: 0/10 * * * * *
+    type: gauge
+    chains: all
+    variants:
+${relayers.map(({ rateFeed }) => `      - [RelayerSigner${rateFeed}]`).join("\n")}
+
+################################################################
+# Copy/paste the relevant values above into aegis' config.yaml #
+################################################################
+`;
+}
+
+void aegisExport();
