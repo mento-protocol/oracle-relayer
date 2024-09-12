f668c86f54b7a68b532ce8f947d75d9d6ead75fd
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2024-09-12 10:27:02 +0200

    fix: missing secret in env

--- infra/local-dotenv-file.tf
+++ infra/local-dotenv-file.tf
@@ -3,6 +3,7 @@ resource "local_file" "env_file" {
   content  = <<-EOT
     GCP_PROJECT_ID=${module.oracle_relayer.project_id}
     DISCORD_WEBHOOK_URL=${var.discord_webhook_url}
+    DISCORD_WEBHOOK_URL_SECRET_ID=${var.discord_webhook_url_secret_id}
     RELAYER_MNEMONIC_SECRET_ID=${var.relayer_mnemonic_secret_id}
   EOT
 }
