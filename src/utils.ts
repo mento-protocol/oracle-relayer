63e50e27396a4e192b481aec44748471ed8ae00b
chapati <philip.paetz@me.com>
2024-09-16 13:42:14 +0200

    Added aegis export script (#22)
    ### Description
    To make it easier to add newly deployed relayed rate feeds to aegis for
    monitoring purposes, this PR adds a `npm run aegis:export` script that
    will print out the necessary aegis `config.yaml` values so we can easily
    copy/paste them over to aegis.
    
    ### Why does this need to be in this repo?
    Because we derive all signer wallet addresses from a mnemonic which is
    managed by the oracle relayer project. And that derivation logic also
    lives here. And deploying new relayers also happens here. So it felt
    kinda natural to at least offer the aegis example as a logical next
    step, because after deploying you likely want to set up monitoring.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { getAddress, keccak256, toHex } from "viem";
 import { HDAccount, mnemonicToAccount } from "viem/accounts";
 
 import { createHash } from "crypto";
@@ -23,4 +24,28 @@ export function deriveRelayerAccount(
   const accountIndex = parseInt(hash, 16) % 2 ** 31;
 
   return mnemonicToAccount(mnemonic, { accountIndex });
+}
+
+/**
+ * A JavaScript implementation of our rateFeed ID generation algorithm.
+ * Original solidity code is: address(uint160(uint256(keccak256(abi.encodePacked(rateFeed)))))
+}
+ * @param rateFeed The rate feed name in string format, i.e. "PHPUSD" or "CELOPHP"
+ * @returns The rate feed ID in address format, i.e. "0xab921d6ab1057601A9ae19879b111fC381a2a8E9" for PHPUSD
+ */
+export function toRateFeedId(rateFeed: string) {
+  // 1. Calculate keccak256 hash
+  const hashedBytes = keccak256(toHex(rateFeed));
+
+  // 2. Convert to BigInt (equivalent to uint256)
+  const hashAsBigInt = BigInt(hashedBytes);
+
+  // 3. Mask to 160 bits (equivalent to uint160)
+  const maskedToUint160 = hashAsBigInt & ((1n << 160n) - 1n);
+
+  // 4. Convert to address (hex string)
+  const addressHex = "0x" + maskedToUint160.toString(16).padStart(40, "0");
+
+  // 5. Return calculated rate feed ID
+  return getAddress(addressHex);
 }
