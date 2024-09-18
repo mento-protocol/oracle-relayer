8e66f890c67ddafab462e352a41cb355360ae094
Philip Paetz <philip.paetz@me.com>
2024-09-18 12:13:29 +0200

    fix: linter errors

--- bin/is-local-function-working.sh
+++ bin/is-local-function-working.sh
@@ -3,12 +3,16 @@ set -e          # Fail on any error
 set -o pipefail # Ensure piped commands propagate exit codes properly
 set -u          # Treat unset variables as an error when substituting
 
-# Script used in the git pre-push hook to check if the local cloud function can start
+#######################################################################################
+# Script used in the git pre-push hook to check if the local cloud function can start #
+#######################################################################################
 
 # Function to kill the background process
 cleanup() {
-	if [ ! -z "$PID" ]; then
-		kill $PID 2>/dev/null
+	# trunk-ignore(shellcheck/SC2317)
+	if [[ -n ${PID} ]]; then
+		# trunk-ignore(shellcheck/SC2317)
+		kill "${PID}" 2>/dev/null
 	fi
 }
 
@@ -23,7 +27,7 @@ PID=$!
 sleep 5
 
 # Check if the process is still running
-if kill -0 $PID 2>/dev/null; then
+if kill -0 "${PID}" 2>/dev/null; then
 	printf "\n✅ Local Function started successfully.\n"
 	exit 0
 else
